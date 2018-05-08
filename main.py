add9a3c9b86fdf5fae099dbd42e8f96cda5557e9
s3raph1m@gmail.com <s3raph1m@gmail.com>
2018-05-08 22:36:17 +0100

    added gitignore to not upload database

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ chatbot = ChatBot("AiMee")
 response = chatbot.get_response("Good morning!")
 print(response)
 
+from chatterbot import ChatBot
+
+chatbot = ChatBot('natsuki-bot')
+
+# Get a response to an input statement
+chatbot.get_response("Hello, how are you today?")
